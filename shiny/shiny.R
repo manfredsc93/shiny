0afebd7d9e0ad829fcb834d52884a72f488a09b3
manfredsc93 <111821370+manfredsc93@users.noreply.github.com>
2022-11-21 18:45:09 -0600

    Add files via upload

---
+++ shiny/shiny.R
@@ -0,0 +1,7 @@
+# Instalación de shiny
+install.packages("shiny")
+
+# Instalación de rsconnect,
+# para conectarse a shinyapps.io, RStudio Connect y otras plataformas similares
+install.packages("rsconnect")
+
